a76e5414d9c216353c3a15929035ce8f202cfce3
Fix85 <klimovartem52@gmail.com>
2026-07-15 18:00:41 +0500

    Port to Minecraft 26.2

--- src/client/java/dev/fix85/BeautifulTextModMenu.java
+++ src/client/java/dev/fix85/BeautifulTextModMenu.java
@@ -3,11 +3,11 @@ package dev.fix85;
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
 import dev.fix85.config.BeautifulTextConfig;
-import me.shedaniel.autoconfig.AutoConfig;
+import me.shedaniel.autoconfig.AutoConfigClient;
 
 public class BeautifulTextModMenu implements ModMenuApi {
     @Override
     public ConfigScreenFactory<?> getModConfigScreenFactory() {
-        return parent -> AutoConfig.getConfigScreen(BeautifulTextConfig.class, parent).get();
+        return parent -> AutoConfigClient.getConfigScreen(BeautifulTextConfig.class, parent).get();
     }
 }
